b6bd39deeb33f1801dc6f4bd16e1fd4db799a415
kurisu1901 <konyahoshi@qq.com>
2020-04-13 22:19:13 +0800

    二叉树深度，js promise

--- test.js
+++ test.js
@@ -1,13 +1,49 @@
-function Animals() {
-
+Promise.all = (arr) => {
+  return new Promise(function(resolve, reject) {
+    if (!Array.isArray(arr)) {
+      reject(new Error('参数必须是数组'))
+    }
+    let result = []
+    let i = 0;
+    try {
+      for (let p of arr) {
+        p.then((val) => {
+          result.push(val)
+          i++
+          if (i === arr.length) {
+            resolve(result)
+          }
+        }).catch(reason => {
+          reject(reason)
+        })
+      }
+    } catch(e) {
+      reject(e)
+    }
+  })
 }
-Animals.prototype.run = function() {
-	console.log('i say run')
+Promise.race = (arr) => {
+  return new Promise(function(resolve, reject) {
+    if (!Array.isArray(arr)) {
+      reject(new Error('参数必须是数组'))
+    }
+    try {
+      for (let p of arr) {
+        p.then((val) => {
+          resolve(val)
+        })
+      }
+    } catch(e) {
+      reject(e)
+    }
+  })
 }
-
-function Dog() {
-
-}
-Dog.prototype = Object.create(Animals.prototype)
-Dog.prototype.constructor = Dog
-
+// value
+// reason
+// status
+// resolve(value)
+// reject(reason)
+// onFulfillCallback()
+// onRejectCallback()
+// then()
+// resolutionPrecedure(x, promise, resolve, reject)
