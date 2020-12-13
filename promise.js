b9c2f4d176e4cca7f7c54b9c617859d947eca95a
Akane <konyahoshi@gmail.com>
2020-12-13 14:39:22 +0800

    手写promise step 2 处理异步情况,将回调函数存储到数组

--- promise.js
+++ promise.js
@@ -6,16 +6,25 @@ function _Promise(excutor) {
   this.status = PENDING
   this.value = undefined
   this.reason = undefined
+  // resolve 和 reject 在异步情况下，then 会先执行，可以在then中先把回调函数先存起来
+  this.onResolveCallbacks = []
+  this.onRejectCallBacks = []
   let resolve = (val) => {
     if (this.status === PENDING) {
       this.status = FULFILLED
       this.value = val
+      this.onResolveCallbacks.forEach(fn => {
+        fn()
+      })
     }
   }
   let reject = (val) => {
     if (this.status === PENDING) {
       this.status = REJECTED
       this.reason = val
+      this.onRejectCallBacks.forEach(fn => {
+        fn()
+      })
     }
   }
   try {
@@ -33,11 +42,17 @@ _Promise.prototype.then = function (resFn, rejFn) {
   if (this.status === REJECTED) {
     rejFn(this.reason)
   }
+  if (this.status === PENDING) {
+    this.onResolveCallbacks.push(() => { resFn(this.value) })
+    this.onRejectCallBacks.push(() => { resFn(this.reason) })
+  }
 }
 
 let a = new _Promise((res, rej) => {
-  console.log(1)
-  res('hhleo')
+  setTimeout(() => {
+    console.log(1)
+    res('异步任务回调')
+  }, 5000)
 })
 
 a.then(val => {
