ed74951ed935eedadf30823518fb6ddde869090f
Akane <konyahoshi@gmail.com>
2020-12-13 14:19:58 +0800

    手写promise step 1

--- promise.js
+++ promise.js
@@ -1,19 +1,45 @@
-// Promise.resolve = function(data) {
-//   if (data.then) {
-//     return Promise.resolve(new Promise((resolve, reject) => {
-//       data.then(resolve)
-//     }))
-//   }
-// }
+// 三个状态：PENDING、FULFILLED、REJECTED
+const PENDING = 'PENDING';
+const FULFILLED = 'FULFILLED';
+const REJECTED = 'REJECTED';
+function _Promise(excutor) {
+  this.status = PENDING
+  this.value = undefined
+  this.reason = undefined
+  let resolve = (val) => {
+    if (this.status === PENDING) {
+      this.status = FULFILLED
+      this.value = val
+    }
+  }
+  let reject = (val) => {
+    if (this.status === PENDING) {
+      this.status = REJECTED
+      this.reason = val
+    }
+  }
+  try {
+    excutor(resolve, reject)
+  } catch (error) {
+    reject(error)
+  }
+}
+_Promise.prototype.then = function (resFn, rejFn) {
+  resFn = typeof resFn === 'function' ? resFn : function () {}
+  rejFn = typeof rejFn === 'function' ? rejFn : function () {}
+  if (this.status === FULFILLED) {
+    resFn(this.value)
+  }
+  if (this.status === REJECTED) {
+    rejFn(this.reason)
+  }
+}
 
-Promise.resolve(new Promise((resolve, reject) => {
+let a = new _Promise((res, rej) => {
   console.log(1)
-  resolve('ya hello')
-}))
-console.log(2)
+  res('hhleo')
+})
 
-// Promise.resolve({
-//   then: function(res, rej) {
-//     console.log('yahaha')
-//   }
-// })
+a.then(val => {
+  console.log('then res' + val)
+})
